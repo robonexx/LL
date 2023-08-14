f0b79c8dcf2ba4be2326c1a78e227ca9d347d1e4
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-08-14 16:48:04 +0200

    fixex preview issue, not updating

--- components/posts/Post.tsx
+++ components/posts/Post.tsx
@@ -9,13 +9,13 @@ export default function Post({ post }: { post: PostType }) {
     <main className='p-10 container mx-auto prose prose-lg'>
       <header className='text-center'>
         <h1 className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold'>
-          {post.title}
+          {post?.title}
         </h1>
       </header>
       <div>
         <Image
-          src={post.image}
-          alt={post.title}
+          src={post?.image}
+          alt={post?.title}
           width={400}
           height={400}
           /*  className='m-auto mt-10 border-2 border-gray-700 object-cover rounded-xl' */
@@ -23,7 +23,7 @@ export default function Post({ post }: { post: PostType }) {
         />
       </div>
       <div className='text-lg text-gray-700 mt-5 text-justify px-20'>
-        <PortableText value={post.body} />
+        <PortableText value={post?.body} />
       </div>
     </main>
   );
