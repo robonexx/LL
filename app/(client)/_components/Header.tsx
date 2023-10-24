2a69bc7d7e8ed819fb15d06ffdbda0116c347824
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-10-24 15:40:22 +0200

    removed use client from layout

--- app/(client)/_components/Header.tsx
+++ app/(client)/_components/Header.tsx
@@ -18,8 +18,8 @@ const Header = () => {
       className={cn(
         `sticky inset-x-0 py-2 px-4 top-0 z-30 w-full transition-all border-b border-gray-200`,
         {
-          'border-b border-gray-200 bg-white/75 backdrop-blur-lg': scrolled,
-          'border-b border-gray-200 bg-white': selectedLayout,
+          'border-b border-gray-200 bg-white/25 backdrop-blur-md': scrolled,
+          'border-b border-gray-200 bg-transparent': selectedLayout,
         },
       )}
     >
