79bee6204931ec5831dbb2a15691bcb937e28495
user.name "robonexx <user.email "robertwagar@gmail.com>
2024-02-12 21:27:55 +0100

    link back from studio to website

--- components/StudioNavbar.tsx
+++ components/StudioNavbar.tsx
@@ -1,15 +1,14 @@
 'use client';
 import React from 'react';
-import { Link } from 'sanity/router';
-import { ArrowUturnLeftIcon } from '@heroicons/react/24/solid';
 import { useRouter } from 'next/navigation';
+import { ArrowUturnLeftIcon } from '@heroicons/react/24/solid';
 
 const StudioNavbar = (props: any) => {
   const { renderDefault } = props;
   const router = useRouter()
   return (
     <div>
-      <button  className='flex items-center p-4 text-white' onClick={() => router.push(`${process.env.NEXT_PUBLIC_BASE_URL}`)}>
+      <button  className='flex items-center p-4 text-white'onClick={() => router.push('/')} >
         <ArrowUturnLeftIcon className='h-4 w-4 fill-white mr-2' /> Back to
         website
       </button>
