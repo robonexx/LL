9f6fd1c52b2e2b366b6758de39f9845efbd0d588
user.name "robonexx <user.email "robertwagar@gmail.com>
2024-02-12 08:59:03 +0100

    ...

--- app/(client)/knowledge/pioneers/page.tsx
+++ app/(client)/knowledge/pioneers/page.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import HallOfFame from './_components/HallOfFame'
-import Headline from '@/components/headline/Headline'
+import React from 'react';
+import HallOfFame from './_components/hallOfFame';
+import Headline from '@/components/headline/Headline';
 
 const Curriculum = () => {
   return (
     <div className='flex flex-col mt-32 md:mt-20 px-8 md:px-20 text-base'>
-     <Headline title='Hall of Fame'/>
+      <Headline title='Hall of Fame' />
       <HallOfFame />
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Curriculum
+export default Curriculum;
